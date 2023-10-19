334e30ce1a5b8a7368e1e23d0e2a1d6aa1ff9e46
WooSangmine <2021232@sungil-i.kr>
2023-10-19 10:50:05 +0900

    Repository(not update)
    Name, CreateDate not update

--- Repository/src/main/java/com/example/demo/Person.java
+++ Repository/src/main/java/com/example/demo/Person.java
@@ -2,10 +2,7 @@ package com.example.demo;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
@@ -15,8 +12,10 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;   //아이디
     private String title;   //제목
+    @Column( updatable=false)
     private LocalDateTime created; //생성일
-    private LocalDateTime updated;   //수정일
+    private LocalDateTime updated;   //수정일'
+    @Column( updatable=false)
     private String name;    //작성자
     private String subtitle;    //부제목(설명)
     private String content; //내용
